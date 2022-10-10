cafdfe4bc6e58cd9abcccd5143bdfa8c2001513f
Grazielly <grazelly13@hotmail.com>
2022-10-09 23:41:36 -0300

    tela de cadastro usuária feita

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,15 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React, { useState } from 'react';
-import { Text, Button, TouchableOpacity, View, Alert } from 'react-native';
+import { Text, TouchableOpacity, View, Alert, Image } from 'react-native';
 import { firebaseConfig } from '../../../back-end/firebase-config';
 import { Inputs } from '../../components/Input';
 import { styles } from './styles';
+import {useNavigation} from '@react-navigation/native';
+import { propsStack } from '../../Models';
 
 export function Cadastro() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
+    const navigation = useNavigation<propsStack>();
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
     const handleCreateAccont =() =>{
@@ -26,24 +29,48 @@ export function Cadastro() {
     }
     return(
         <View style={styles.container}>
-        
+
+        <Image style={styles.imagem} source={require('../../assets/imag_test.png')} />
+
+        <Text style={styles.label_input}>Nome:</Text>
         <Inputs 
-        titlo='E-mail'
+        titlo='Nome completo'
+        onChangeText={(text)=> setEmail(text)}
+        />  
+        
+        <Text style={styles.label_input}>E-mail:</Text>
+        <Inputs 
+        titlo='maria@gmail.com'
         onChangeText={(text)=> setEmail(text)}
         />
+
+        <Text style={styles.label_input}>Senha:</Text>
         <Inputs 
-        titlo='Senha'
+        titlo='Escolha uma senha'
         onChangeText={(text)=> setSenha(text)}
         secureTextEntry
         />
 
-      
-       <TouchableOpacity  
-       onPress={handleCreateAccont} 
-       style={styles.btn}>
-        <Text>cadastro</Text>
-        </TouchableOpacity>
-         
+        <Text style={styles.label_input}>Confirmar Senha:</Text>
+        <Inputs 
+        titlo='Confirme sua Senha'
+        onChangeText={(text)=> setSenha(text)}
+        secureTextEntry
+        />
+
+      <View style={styles.btn}>
+      <TouchableOpacity  
+        onPress={handleCreateAccont} 
+        style={styles.btn_cadastrar}>
+        <Text style={styles.label_btn}>Cadastrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity  
+        onPress={()=> navigation.navigate("Login")} 
+        style={styles.btn_cancelar}>
+        <Text style={styles.label_btn}>Cancelar</Text>
+      </TouchableOpacity>
+      </View>
       
       </View>   
     );
--- src/pages/Cadastro/styles.ts
+++ src/pages/Cadastro/styles.ts
@@ -4,18 +4,49 @@ export const styles = StyleSheet.create({
 
     container:{
         flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 18,
+    },
+    imagem:{
+        width: "40%",
+        height: "24%",
+        marginBottom: 20,
+        borderRadius: 8,
+    },
+    label_input:{
+        width: '100%',
+        textAlign: "left",
+        fontSize: 18,
+        marginBottom: 5,
     },
     btn:{
-        textAlign:'center',
-        width: '100%', 
-        height: 40, 
+        width: '100%',
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        marginTop: 30,
+    },
+    btn_cadastrar:{
+        width: '40%',
+        height: 50, 
         marginBottom: 10,
         borderRadius: 4,
-        backgroundColor: '#007EF3',
-        
-},
+        backgroundColor: '#007EF3',    
+        justifyContent: 'center',
+    },
+    btn_cancelar:{
+        width: '40%',
+        height: 50, 
+        marginBottom: 10,
+        borderRadius: 4,
+        backgroundColor: '#D01111', 
+        justifyContent: 'center',
+    },
+    label_btn:{
+        textAlign: 'center',
+        color:'#fff',
+        fontSize: 20,
+    }
 
 
 
