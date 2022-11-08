c7b16f8c43026a2160b5da0c71a81402371b3dc5
willian <batatinha@gmail.com>
2022-11-07 21:32:49 -0300

    criação da pasta EditarEmpresa

---
+++ src/pages/EditarEmpresa/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import { Background } from '../../components/Background';
+
+import { styles } from './styles';
+
+export function EditarEmpresa() {
+  return (
+    <Background>
+      <Header title='Editar Perfil' icone_imag />
+
+      
+    </Background>
+  );
+}
---
+++ src/pages/EditarEmpresa/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  texto:{
+    fontSize: 20,
+    fontWeight: '600',
+  }
+});
